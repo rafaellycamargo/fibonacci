cc2014143b407204314e87592afbc9ff8bef25b1
Rafaelly Camargo <rafaelly.c@aluno.ifsp.edu.br>
2020-08-24 15:29:29 -0300

    Adicionei index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Fibonacci</title>
+    </head>
+
+    <body>
+        <form action="fibonacci.php" method="GET">
+            <fieldset>
+            <legend>Quantidade de números da sequência</legend>
+            <input type="number" name="quant" />
+            <br/> <br/>
+
+            <input type="submit" align="center">
+            </fieldset>
+        </form>
+    </body>
+</html>
